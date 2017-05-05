cea7ca2f85945f8bddd1b8fa70ffc5de0c171ca9
yinxueyuan <838476901@qq.com>
2017-05-05 14:09:38 +0800

    feat: bagpipe

--- src/异步并发控制/bagpipe/bagpipe.js
+++ src/异步并发控制/bagpipe/bagpipe.js
@@ -1,21 +1,25 @@
 const Bagpipe = require('bagpipe');
 const fs = require('fs');
 
-let bagpipe = new Bagpipe();
+let bagpipe = new Bagpipe(10, {
+  refuse: true
+});
 
 function async () {
-  fs.readFile('file1.txt', 'utf8', function(err, content) {
+  fs.readFile('file1.txt', 'utf8', function (err, content) {
     if (err) {
       console.error(err)
       return false;
-    }
-    console.log(content);
+    };
+    setTimeout(function () {
+      console.log(content)
+    }, 3000);
   })
 }
 
 for (let i = 0; i < 100; i++) {
-  bagpipe.push(async, function () {
-    console.log(i);
+  bagpipe.push(async, function (err) {
+    // console.error(err);
   })
 }
 
